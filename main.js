e56fed64e03196bfea3693251e29d24f161b8306
Your Name <you@example.com>
2023-01-29 17:52:10 +0200

    first commit

---
+++ main.js
@@ -0,0 +1,22 @@
+/*1*/
+console.log('Hello world');
+/*2*/
+alert('Hello World');
+/*3*/
+prompt('Are you ok?');
+
+var task3 = prompt('Rly ok?');
+console.log((task3));
+/*4*/
+confirm('Пішли вчитись?')
+var result = confirm('Домашка виконана?');
+console.log(result);
+/*5*/
+var questionOne = prompt('Який поточний рік?');
+var questionTwo = prompt('Який рік народження?')
+alert('Вам '+ (questionOne - questionTwo) + ' років' );
+
+
+
+
+
