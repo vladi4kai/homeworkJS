8a67cc1d3510dae3c7207554f8c4a6d0d84f4c04
Your Name <you@example.com>
2023-01-31 22:56:04 +0200

    homework

--- main.js
+++ main.js
@@ -1,20 +1,54 @@
-/*1*/
-console.log('Hello world');
-/*2*/
-alert('Hello World');
-/*3*/
-prompt('Are you ok?');
+//1
+var notation = 'Burger'
+let animal = 'Monkey'
+const numberOfBurgers = 12
+//2-3 task
+var stringType = 'Line'
+console.log(typeof stringType)
+stringType = Number(stringType)
+console.log(typeof stringType)
 
-var task3 = prompt('Rly ok?');
-console.log((task3));
-/*4*/
-confirm('Пішли вчитись?')
-var result = confirm('Домашка виконана?');
-console.log(result);
-/*5*/
-var questionOne = prompt('Який поточний рік?');
-var questionTwo = prompt('Який рік народження?')
-alert('Вам '+ (questionOne - questionTwo) + ' років' );
+let numberType = 232
+console.log(typeof numberType)
+numberType = String(numberType)
+console.log(typeof numberType)
+
+let booleanType = true
+console.log(typeof booleanType)
+booleanType = String(booleanType)
+console.log(typeof booleanType)
+
+console.log(typeof null)
+console.log(typeof 20 / 0)
+console.log(typeof [1,2,3])
+function reverse(){}
+console.log(typeof reverse())
+// console.log(typeof ) = ERROR
+console.log(typeof moloko / 150)
+//4
+let priceOfArticle = 40
+let nameOfAcrticle = 'egg'
+let monthOfPurchase = 'july'
+
+priceOfArticle = 70
+monthOfPurchase = 'september'
+//5
+const amountOfArticle = 3
+const totalPrice = priceOfArticle * amountOfArticle
+console.log(totalPrice)
+//6
+const message = 'Я купив '+ amountOfArticle +' лотків за ціною '+ totalPrice +'. Ціна одного лотка становить '+ priceOfArticle +''
+console.log(message)
+
+
+
+
+
+
+
+
+
+
 
 
 
