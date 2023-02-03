16740e0566e1aed36970baca4c9ca1333308ae66
Vladyslav <121558615+vladi4kai@users.noreply.github.com>
2023-02-03 19:26:55 +0200

    homework3

--- main.js
+++ main.js
@@ -1,52 +1,75 @@
+//1
+let firstType = 'banana'
+let secondType = `bananana`
+let thirdType = "banananana"
 //2
-let task2 = null
-console.log(task2)
-console.log(typeof null)
-//3
-let task3 
-console.log(task3)
-console.log(typeof undefined)
-//4 
-let task4First = 'Burger'
-let task4Second = 150
-let task4Three =  task4First / task4Second
-console.log(task4Three)
-console.log(typeof NaN)
-
-if (isNaN(task4Three)){
-    console.log(true)
-} else {
-    console.log(false)
-}
-//5
-let firstNumber = +prompt('Введіь перше число')
-let secondNumber = +prompt('Введіь друге число')
-let thirdNumber = +prompt('Введіь третє число')
-
-let result = firstNumber + secondNumber + thirdNumber
+let firstNumber = '1'
+let secondNumber = 1
+let result = firstNumber + secondNumber
 console.log(result)
 
-function checkDataType(){
-    if (isNaN(result))
-        console.log('This is NaN')
-      else {
-        console.log('This is number')  
-    }
-}
-console.log(checkDataType())
+let firstQuestion = 'How '
+let secondQuestion = 'are you?'
+let result2 = firstQuestion + secondQuestion
+console.log(result2)
+
+let myHobbies = 'reading books, speaking with friends, playing volleyball'
+let result3 = `My hobbies are ${myHobbies}`
+console.log(result3)
+
+let myName = "Vlad"
+// alert('Hello my name is ' + myName)
+//3
+// let infoName = prompt('Whats your name?')
+// let infoAge = prompt('How old are you?')
+// let infoHobbies = prompt('What are you like doing?')
+// let infoTravel = prompt('Did you travel a lot before the war?')
+// let infoFuture = prompt('What are you going to do at first, when war ends')
+// let infoAboutStudy = prompt('How much time can you allocate to learn something new?')
+// let result4 = infoName + " " + infoAge + " " + infoHobbies + " " + infoTravel + " " + infoFuture + " " + infoAboutStudy
+// console.log(result4)
+//4
+let nameData = 'Oleksandr'
+console.log(nameData.length)
+console.log(nameData.toLocaleLowerCase())
+console.log(nameData.toLocaleUpperCase())
+//5
+let numberData = 123956
+numberData = String(numberData)
+console.log(numberData)
+console.log(typeof numberData)
+//6
+//+Infinity
+let forInfinityOne = 1 / 0
+console.log(forInfinityOne)
+console.log(Infinity + Infinity)
+console.log(Infinity * Infinity)
+console.log(3 * Infinity)
+//-Infinity
+let forInfinityTwo = -5 / 0
+console.log(forInfinityTwo)
+console.log(-Infinity * 5)
+//7
+nameData = Number(nameData)
+console.log(typeof nameData)
+console.log(String(nameData))
+
+numberData = String(numberData)
+console.log(typeof numberData)
+console.log(numberData)
+//8
+let mathNumber = 22 / 21
+let oneNumber
+let twoNumber
+//
+oneNumber = Math.ceil(mathNumber)
+console.log(Math.pow(oneNumber, 2))
+//
+twoNumber = Math.floor(mathNumber)
+console.log(Math.pow(twoNumber, 2))
+//9
+console.log(Math.round(Math.random()*100+1))
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
+   
 
