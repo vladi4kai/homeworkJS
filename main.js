822accbaf254cebc0305bc68816ab5c2c936e816
Your Name <you@example.com>
2023-03-06 20:27:00 +0200

    homework8

--- main.js
+++ main.js
@@ -1,125 +1,86 @@
 //1
 
-// Масив який треба сортувати
-let arrayOne = [1, 6, 3, 8, 7, 9, 10]
+// спосіб 1
 
-// По зростанню
-
-function sorterOne(a, b){
-    return a - b
+let aboutOne = {
+    name: 'John',
+    age:30,
 }
-arrayOne.sort(sorterOne)
-console.log(arrayOne)
 
-// По спаданню
+//спосіб 2
 
-function sorterTwo(c, d){
-    return d - c
-}
-arrayOne.sort(sorterTwo)
-console.log(arrayOne)
+let aboutTwo = new Object()
+
+aboutTwo.name = 'Max'
+aboutTwo.age = 23
 
 //2
-// Метод перший - самий кращий та зрозумілий
 
-let unfiltredArray = [1,2,[3,4],[5,[6,7]]]
-let filtredArrayOne = unfiltredArray.flat(2)
-
-console.log(filtredArrayOne)
-
-// Метод два - через метод flatMap
-
-let filtredArrayTwo = unfiltredArray.flatMap(arrFilter => Array.isArray(arrFilter) ? arrFilter : [arrFilter]).flat()
-console.log(filtredArrayTwo)
+const newUserData = {
+    name: 'Oleh',
+    age: 25,
+    "new proffesion": 'programmer',
+}
+console.log(newUserData.age)
+console.log(newUserData.name)
+console.log(newUserData["new proffesion"])
 
 //3
 
-let arrThree = [1,3,3,6,8,6,1]
-
-let arrThreeUniqueItems = new Set(arrThree)
-console.log(arrThreeUniqueItems)
+let object = {
+    name: 'Oleh',
+    age: 25,
+    hobby: 'readins books'
+}
+for (let i in object){
+    console.log(i + ':' + object[i])
+}
 
 //4
 
-let arrFour = [1, 6, 8, 6, 1]
-
-for (let i = 0; i < arrFour.length; i++){
-    arrFour[i]+=5
+let infoAboutYou = {
+    hobby: 'learn something new',
 }
-console.log(arrFour)
+infoAboutYou.proffesion = 'future programmer'
+infoAboutYou.age = 19
+infoAboutYou.name = 'Vlad'
 
-let arrFourSum = 0
+infoAboutYou.hobby = 'reading books'
 
-for (let i = 0; i < arrFour.length; i++){
-    arrFourSum += arrFour[i]
-}
-console.log(arrFourSum)
+infoAboutYou.newInfo = true
+delete infoAboutYou.newInfo
 
 //5
 
-let five = 6
-// створюємо масив від 1 до 6 через map. перед тим присвоєїм нашій змінній масив
-let arrFiveTwo = new Array(five).fill().map((value, i) => i + 1)
-console.log(arrFiveTwo)
-// обчислюємо факторіал
-let factorial = arrFiveTwo.reduce((a, b) => a * b)
-console.log(factorial)
+const userData = {
+    name: 'Oleh',
+    age: 25,
+}
+
+let copyUserData = {... userData}
+copyUserData.age = 20
+console.log(copyUserData)
+console.log(userData)
 
 //6
 
-function palindrome(anyWord){
-    anyWord = anyWord.toLowerCase()
-
-    let reverseItem = anyWord.split('').reverse().join('')
-
-    return anyWord === reverseItem
+function toArray(obj) {
+   return Object.entries(obj)
 }
-console.log(palindrome('racecar'))
-console.log(palindrome('Анна'))
-console.log(palindrome('cat'))
+console.log(toArray({a: 1 , b: 2}))
+console.log(toArray({shrimp: 15 , tots: 22}))
+console.log(toArray({}))
 
 //7
 
-function anagrammaChecker(wordA, wordB){
-    if (wordA.length !== wordB.length){
-        return false
-    }
-    wordA = wordA.toLowerCase()
-    wordB = wordB.toLowerCase()
-    wordA = wordA.split('').sort().join('')
-    wordB = wordB.split('').sort().join('')
+const array = ['orange', 'pineapple', 'orange', 'apple', 'pineapple', 'pineapple', 'orange', 'pineapple','orange', 'pineapple', 'pineapple']
 
-    return wordA === wordB
-}
-console.log(anagrammaChecker('friend', 'finder'))
-console.log(anagrammaChecker('sharm', 'marsh'))
-console.log(anagrammaChecker('cat', 'dog'))
+let counterOfItems = array.reduce((calc, curr) => { calc[curr] = (calc[curr] || 0) + 1
+    return calc
+},{})
 
+const uniqueItems = [...new Set(array)]
 
-//8
+uniqueItems.sort((a, b) => counterOfItems[b] - counterOfItems[a])
 
-function checkVowelOrConsonent(array){
-    let arrVowel = []
-    let arrConsonent = []
-    for (let i = 0; i < array.length; i++){
-        if (['a','e','i','o','u'].includes(array[i].toLowerCase())){
-            arrVowel.push(array[i])
-        } else {
-            arrConsonent.push(array[i])
-        }
-    }
-    arrVowel.sort()
-    arrConsonent.sort()
-
-    arrVowel.push(arrVowel.length)
-    arrConsonent.push(arrConsonent.length)
-
-    return [arrVowel, arrConsonent]
-}
-console.log(checkVowelOrConsonent(['a', 'f', 'd', 'j', 'y', 't', 'i', 'r']))
-
-
-
-
-
-
+console.log(uniqueItems)
